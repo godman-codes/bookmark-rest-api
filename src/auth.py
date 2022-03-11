7428bc6cd157cbeb97f332708f154cffcbb9b5c0
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-03-11 00:31:38 +0000

    using swagger and flasgger

--- src/auth.py
+++ src/auth.py
@@ -1,13 +1,10 @@
-import http
-from os import access
-from xml.dom.minidom import Identified
 from flask import Blueprint, jsonify, request
 from werkzeug.security import check_password_hash, generate_password_hash
 from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 import validators
 from src.database import User, db
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
-
+import os
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,4 +1,3 @@
-from urllib.parse import urlunsplit
 from flask import Blueprint, current_app, jsonify, request
 import validators
 from flask import jsonify
@@ -12,7 +11,6 @@ bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
 @bookmarks.route("/", methods=['POST', 'GET'])
 @jwt_required()
 def handle_bookmarks():
-
    current_user = get_jwt_identity()
    
    if request.method == "POST":
@@ -147,3 +145,25 @@ def delete_bookmarks(id):
    db.session.commit()
 
    return jsonify({}), HTTP_204_NO_CONTENT
+
+
+@bookmarks.get('/stats')
+@jwt_required()
+def get_stats():
+
+   current_user = get_jwt_identity()
+   
+   data=[]
+   
+   items = Bookmarks.query.filter_by(user_id=current_user).all()
+
+   for item in items:
+      new_link={
+         'visits': item.visits,
+         'url': item.url,
+         'id': item.id,
+         'short_url': item.short_url
+      }
+
+      data.append(new_link)
+   return jsonify({'data': data}), HTTP_200_OK
